0f648b60b24205182a902f04824a0da6cda5582c
asifhasan002 <asifannada@gmail.com>
2025-04-17 14:21:36 +0600

    initial commit

---
+++ iOS-Auto-Translator/iOS_Auto_TranslatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_Auto_TranslatorApp.swift
+//  iOS-Auto-Translator
+//
+//  Created by Md Asif Hasan on 17/4/25.
+//
+
+import SwiftUI
+
+@main
+struct iOS_Auto_TranslatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iOS-Auto-TranslatorTests/iOS_Auto_TranslatorTests.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_Auto_TranslatorTests.swift
+//  iOS-Auto-TranslatorTests
+//
+//  Created by Md Asif Hasan on 17/4/25.
+//
+
+import Testing
+@testable import iOS_Auto_Translator
+
+struct iOS_Auto_TranslatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
