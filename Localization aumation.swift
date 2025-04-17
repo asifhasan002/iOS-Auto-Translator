1a2b111f25f9fd0d149ad34871c345009051cdab
asifhasan002 <asifannada@gmail.com>
2025-04-17 14:30:28 +0600

    Localization aumation

---
+++ Localization aumation.swift
@@ -0,0 +1,52 @@
+1. localization swift file
+
+2. add language in array []
+
+3. change string from auto translation
+
+3. csv/xcl create automatically from localization swift
+
+4. csv/xcl modify update which should update swift localization file
+
+5. auto add new language in every case if a new language add in array
+                                            
+6. add new language case with all languages automatically
+
+6. show missing localiztion. eg. "upload", or for chinese/ "chinese language of upload1"
+
+7. Create new case string automatically.
+                                            
+8. ignore string case creation
+                                            
+9. string like "aaa\(strng)" localization
+
+10. dynamic localization number update in app with + - button
+
+11. server string localization
+                                            
+12. if server string only en, or missing some language then it search it localization swift file, but server string should not create automatically, print that server string missing if not found but auto translation
+                                            
+13. ignore string case. eg "chinese language of upload1"
+                                            
+14. update language from settings
+                                            
+15. app launches after update language from settings
+                                            
+16. show
+                                            
+17. missing percentage of localization
+ 
+18. show unused localization
+
+19. Ignore in full case string
+                                            
+20. Create case string one time
+
+21. force avoid string use
+                                            
+22.date
+
+1. extension: auto translation off-on
+2. Server autotranslation full match string, partial match string off-on
+3. Auto create case string
+4.
